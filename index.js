eea0eacae98731a3add1ce470c576d3ef600ee55
Andrew McIndoe <amcindoe@powerhrg.com>
2021-05-25 10:08:00 -0400

    loops, loops

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = [ 'Lisa', 'Kaitlin', 'Jan']
+
+function writeCards(names, event){
+   const bdaynames = []
+    for(let i = 0; i < names.length; i++){
+        bdaynames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return bdaynames
+}
+
+writeCards(names);
+
+function countDown(number){
+    while (number >= 0) 
+      console.log(number--);
+  }  
